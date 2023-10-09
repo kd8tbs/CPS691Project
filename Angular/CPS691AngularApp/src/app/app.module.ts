1224666ad2e56a518d4266fc80779b387edd2629
Jared Dunham <jaredscottdunham@gmail.com>
2023-10-09 18:39:58 -0400

    Adding initial angular components

--- Angular/CPS691AngularApp/src/app/app.module.ts
+++ Angular/CPS691AngularApp/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { ButtonComponent } from './Components/button/button.component';
+import { TextFieldComponent } from './Components/text-field/text-field.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ButtonComponent,
+    TextFieldComponent
   ],
   imports: [
     BrowserModule,
