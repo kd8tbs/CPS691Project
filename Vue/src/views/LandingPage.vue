3fd9ba220e0300b3bedb5f8b3f13b495952d65e2
Safz14 <146881673+Safz14@users.noreply.github.com>
2023-10-23 21:02:03 -0400

    Added my Vue.js code

---
+++ Vue/src/views/LandingPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="landing-page">
+    <h1>CPS 691 - Project</h1>
+    <p>VUE.JS</p>
+    <router-link to="/bouncing-balls">Bouncing Balls</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LandingPage',
+};
+</script>
