e4d5e4c52c9fa954b5763a8761f81fc217a755ed
Safz14 <146881673+Safz14@users.noreply.github.com>
2023-11-11 02:48:14 -0500

    Changes

--- Vue/vue.config.js
+++ Vue/vue.config.js
@@ -1,10 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const { defineConfig } = require('@vue/cli-service');
 
-module.exports = {
+module.exports = defineConfig({
+  transpileDependencies: true,
   devServer: {
     hot: true,
   },
-};
+});
+
