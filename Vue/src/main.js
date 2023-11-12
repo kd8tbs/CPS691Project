6dc887bbe90b53a022bcabdaafbb33ed41e21ef9
Safz14 <146881673+Safz14@users.noreply.github.com>
2023-11-12 15:49:24 -0500

    Added my Vue Project

--- Vue/src/main.js
+++ Vue/src/main.js
@@ -1,11 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-// import FadeInFadeOut from './components/FadeIn&FadeOut.vue'
 
-const app = createApp(App);
 
-// app.component('fade-in-fade-out', FadeInFadeOut);
+createApp(App).use(router).mount('#app')
 
-app.use(router);
-app.mount('#app');
