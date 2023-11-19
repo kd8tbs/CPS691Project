45602c215ed2b0d6b62a682e8c78ff62050f4eb5
Gary Pinsky <kd8tbs@gmail.com>
2023-11-19 02:02:26 -0500

    made graphs of data

--- Measurement/graph.py
+++ Measurement/graph.py
@@ -1,42 +1,162 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+import csv
 
-# Define the list of CSV file names
-csv_files = [
-    "Measurement\\Data\\angular_performance_results.csv",
-    "Measurement\\Data\\blazor_performance_results.csv",
-    "Measurement\\Data\\react_performance_results.csv",
-    "Measurement\\Data\\vue_performance_results.csv"
-]
 
-# Initialize empty DataFrames to store data from each CSV file
-data_frames = []
 
-# Read and store data from each CSV file
-for csv_file in csv_files:
-    data = pd.read_csv(csv_file)
-    data_frames.append(data)
+class TestData:
+    test_csv_path = None
+    name = None
+    data = None
+    page_load_time = None
+    def __init__(self, test_csv_path, name):
+        self.test_csv_path = test_csv_path
+        self.name = name
+        self.data = pd.read_csv(test_csv_path)
+        self.page_load_time = None
+        with open('page_load_time.csv', 'r') as file:
+            reader = csv.reader(file)
+            next(reader)  # Skip the header
+            for row in reader:
+                if row[1] == self.test_csv_path:
+                    self.page_load_time = float(row[0])
+        
+    def get_average_frame_rate(self):
+        return self.data['Frame Rate (FPS)'].mean()
+    
+    def get_average_memory_usage(self):
+        return self.data['Memory Usage (MB)'].mean()
+    
+    def get_page_load_time(self):
+        return self.page_load_time
+    
+# Create arrays of test data objects
+bouncing_ball_tests = [ TestData('angular_boucing_ball_1k.csv', 'Angular'),
+                        TestData('react_bouncing_balls_1k.csv', 'React'),
+                        TestData('vue_bouncing_balls_1k.csv', 'Vue'),
+                        TestData('blazor_bouncing_balls_1k.csv', 'Blazor')]
 
-# Create a line plot for memory usage over time
-plt.figure(figsize=(12, 6))
-for i, data in enumerate(data_frames):
-    framework_name = os.path.splitext(os.path.basename(csv_files[i]))[0].split('_')[0].capitalize()
-    plt.plot(data['Time (s)'], data['Memory Usage (MB)'], label=framework_name)
+fading_tests = [ TestData('angular_fade_1k.csv', 'Angular'),
+                TestData('react_fade_1k.csv', 'React'),
+                TestData('vue_fade_1k.csv', 'Vue'),
+                TestData('blazor_fade_1k.csv', 'Blazor')]
 
-plt.xlabel('Time (s)')
-plt.ylabel('Memory Usage (MB)')
-plt.title('Memory Usage Over Time')
+pulse_tests = [ TestData('angular_pulse_1k.csv', 'Angular'), 
+                TestData('react_pulse_1k.csv', 'React'),
+                TestData('vue_pulse_1k.csv', 'Vue'),
+                TestData('blazor_pulse_1k.csv', 'Blazor')]
+
+all_tests = bouncing_ball_tests + fading_tests + pulse_tests
+
+# Create bar charts comparing the average frame rate of bouncing ball tests
+bouncing_ball_frame_rates = [test.get_average_frame_rate() for test in bouncing_ball_tests]
+plt.bar([test.name for test in bouncing_ball_tests], bouncing_ball_frame_rates)
+plt.title("Average Frame Rate of Bouncing Ball Tests")
+plt.xlabel("Framework")
+plt.ylabel("Average Frame Rate (FPS)")
+plt.savefig("bouncing_ball_frame_rates.png")
+plt.close()
+# Create bar charts comparing the average frame rate of fading tests
+fading_frame_rates = [test.get_average_frame_rate() for test in fading_tests]
+plt.bar([test.name for test in fading_tests], fading_frame_rates)
+plt.title("Average Frame Rate of Fading Tests")
+plt.xlabel("Framework")
+plt.ylabel("Average Frame Rate (FPS)")
+plt.savefig("fading_frame_rates.png")
+plt.close()
+# Create bar charts comparing the average frame rate of pulse tests
+pulse_frame_rates = [test.get_average_frame_rate() for test in pulse_tests]
+plt.bar([test.name for test in pulse_tests], pulse_frame_rates)
+plt.title("Average Frame Rate of Pulse Tests")
+plt.xlabel("Framework")
+plt.ylabel("Average Frame Rate (FPS)")
+plt.savefig("pulse_frame_rates.png")
+plt.close()
+
+# Create line graph of frame rate over time in bouncing ball tests
+for test in bouncing_ball_tests:
+    plt.plot(test.data['Time (s)'], test.data['Frame Rate (FPS)'], label=test.name)
+plt.title("Frame Rate Over Time (Bouncing Ball Tests)")
+plt.xlabel("Time (s)")
+plt.ylabel("Frame Rate (FPS)")
 plt.legend()
-plt.grid()
-plt.savefig('Measurement\\Data\\memory_usage_over_time.png')  # Save the memory usage plot as an image
+plt.savefig("bouncing_ball_frame_rate_over_time.png")
+plt.close()
 
-# Calculate and create a bar chart for average frame rate
-average_frame_rates = [data['Frame Rate (FPS)'].mean() for data in data_frames]
-plt.figure(figsize=(8, 6))
-framework_names = [os.path.splitext(os.path.basename(csv))[0].split('_')[0].capitalize() for csv in csv_files]
-plt.bar(framework_names, average_frame_rates)
-plt.xlabel('Framework')
-plt.ylabel('Average Frame Rate (FPS)')
-plt.title('Average Frame Rate Comparison')
-plt.savefig('Measurement\\Data\\average_frame_rate_comparison.png')  # Save the average frame rate comparison as an image
+# Create line graph of frame rate over time in fading tests
+for test in fading_tests:
+    plt.plot(test.data['Time (s)'], test.data['Frame Rate (FPS)'], label=test.name)
+plt.title("Frame Rate Over Time (Fading Tests)")
+plt.xlabel("Time (s)")
+plt.ylabel("Frame Rate (FPS)")
+plt.legend()
+plt.savefig("fading_frame_rate_over_time.png")
+plt.close()
+
+# Create line graph of frame rate over time in pulse tests
+for test in pulse_tests:
+    plt.plot(test.data['Time (s)'], test.data['Frame Rate (FPS)'], label=test.name)
+plt.title("Frame Rate Over Time (Pulse Tests)")
+plt.xlabel("Time (s)")
+plt.ylabel("Frame Rate (FPS)")
+plt.legend()
+plt.savefig("pulse_frame_rate_over_time.png")
+plt.close()
+
+# Create bar chart of average page load times per framework
+page_load_times = [test.get_page_load_time() if test.get_page_load_time() is not None else 0 for test in all_tests]
+plt.bar([test.name for test in all_tests], page_load_times)
+plt.title("Average Page Load Time")
+plt.xlabel("Framework")
+plt.ylabel("Page Load Time (s)")
+plt.savefig("page_load_times.png")
+plt.close()
+
+# Create bar chart of average frame rate per framework
+frame_rates = [test.get_average_frame_rate() for test in all_tests]
+plt.bar([test.name for test in all_tests], frame_rates)
+plt.title("Average Frame Rate")
+plt.xlabel("Framework")
+plt.ylabel("Frame Rate (FPS)")
+plt.savefig("frame_rate_all_test_average.png")
+plt.close()
+
+# Create bar chart of average memory usage per framework
+memory_usages = [test.get_average_memory_usage() for test in all_tests]
+plt.bar([test.name for test in all_tests], memory_usages)
+plt.title("Average Memory Usage")
+plt.xlabel("Framework")
+plt.ylabel("Memory Usage (MB)")
+plt.savefig("memory_usage_all_test_average.png")
+plt.close()
+
+# Create memory usage over time line graphs of bouncing ball tests
+for test in bouncing_ball_tests:
+    plt.plot(test.data['Time (s)'], test.data['Memory Usage (MB)'], label=test.name)
+plt.title("Memory Usage Over Time (Bouncing Ball Tests)")
+plt.xlabel("Time (s)")
+plt.ylabel("Memory Usage (MB)")
+plt.legend()
+plt.savefig("bouncing_ball_memory_usage_over_time.png")
+plt.close()
+
+# Create memory usage over time line graphs of fading tests
+for test in fading_tests:
+    plt.plot(test.data['Time (s)'], test.data['Memory Usage (MB)'], label=test.name)
+plt.title("Memory Usage Over Time (Fading Tests)")
+plt.xlabel("Time (s)")
+plt.ylabel("Memory Usage (MB)")
+plt.legend()
+plt.savefig("fading_memory_usage_over_time.png")
+plt.close()
+
+# Create memory usage over time line graphs of pulse tests
+for test in pulse_tests:
+    plt.plot(test.data['Time (s)'], test.data['Memory Usage (MB)'], label=test.name)
+plt.title("Memory Usage Over Time (Pulse Tests)")
+plt.xlabel("Time (s)")
+plt.ylabel("Memory Usage (MB)")
+plt.legend()
+plt.savefig("pulse_memory_usage_over_time.png")
+plt.close()
